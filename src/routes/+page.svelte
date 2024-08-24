ffcf54ab1069562fd7438937294564f4fd5141a4
Alexander Liu <a@alexanderliu.com>
2024-08-23 22:12:16 -0700

    feat: basic colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { Poline } from 'poline';
+
+	const poline = new Poline({
+		anchorColors: [
+			[309, 0.72, 0.8],
+			[67, 0.32, 0.08],
+		],
+		numPoints: 2,
+	});
+</script>
+
+<div class="colors">
+	{#each poline.colorsCSS as color}
+		<div class="color" style:background-color={color} />
+	{/each}
+</div>
+
+<style>
+	.colors {
+		display: flex;
+	}
+
+	.color {
+		width: 100px;
+		height: 100px;
+	}
+</style>
