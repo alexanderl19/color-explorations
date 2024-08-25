ee26f4c92c220c35d8034ad4bdb822ae2249ae7b
Alexander Liu <a@alexanderliu.com>
2024-08-25 15:02:33 -0700

    feat: basic inputs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,22 @@
 <script lang="ts">
+	import { ColorSpace, sRGB, HSL, parse, to } from 'colorjs.io/fn';
 	import { Poline } from 'poline';
 
-	const poline = new Poline({
+	let anchorColor1 = '#4F405E';
+	let anchorColor2 = '#191b0e';
+	let numPoints = 2;
+
+	ColorSpace.register(sRGB);
+	ColorSpace.register(HSL);
+	$: anchorColor1HSL = to(parse(anchorColor1), 'hsl').coords;
+	$: anchorColor2HSL = to(parse(anchorColor2), 'hsl').coords;
+
+	$: poline = new Poline({
 		anchorColors: [
-			[309, 0.72, 0.8],
-			[67, 0.32, 0.08],
+			[anchorColor1HSL[0], anchorColor1HSL[1] / 100, anchorColor1HSL[2] / 100],
+			[anchorColor2HSL[0], anchorColor2HSL[1] / 100, anchorColor2HSL[2] / 100],
 		],
-		numPoints: 2,
+		numPoints,
 	});
 </script>
 
@@ -15,8 +25,27 @@
 		<div class="color" style:background-color={color} />
 	{/each}
 </div>
+<div class="inputs">
+	<label>
+		<span>Anchor Color 1</span>
+		<input type="color" bind:value={anchorColor1} />
+	</label>
+	<label>
+		<span>Anchor Color 2</span>
+		<input type="color" bind:value={anchorColor2} />
+	</label>
+	<label>
+		<span>Number of Points</span>
+		<input type="number" min="1" bind:value={numPoints} />
+	</label>
+</div>
 
 <style>
+	.inputs {
+		margin-top: 1rem;
+		margin-bottom: 1rem;
+	}
+
 	.colors {
 		display: flex;
 	}
